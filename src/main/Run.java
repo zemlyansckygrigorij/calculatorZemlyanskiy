ff2fb791cb0463a30ca9da7433460c56bf6a4245
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2018-10-31 08:18:52 +0400

    Default Changelist

--- src/main/Run.java
+++ src/main/Run.java
@@ -3,17 +3,16 @@ package main;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-//код не доделанный  прошу извинить . много времени ушло на переделку алгоритма.
+//конечный вариант . тестирую
 public class Run {
 
     public static void main(String [] args){
-         String  rad;
-         int r;
+        String  rad;
+        int r;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try{
             rad = br.readLine(); //We read from user's input
-           //123r = Integer.parseInt(rad); //We validate if "rad" is an integer (if so we skip catch call and continue on the next line, otherwise, we go to it (catch call))
-            //*System.out.println(rad);
+
 
             Mathematics math = new Mathematics(rad);
             System.out.println("resultat operation " + math.getResult());
